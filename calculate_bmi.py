b4eecd08cbb0fc31b20333f4cd0b5b0168bc8af8
Ray <cilver2965@naver.com>
2025-12-21 22:55:51 +0900

    Create calculate_bmi.py

---
+++ calculate_bmi.py
@@ -0,0 +1,16 @@
+# 1. BMI(체질량지수) 계산기
+
+height = float(input("키를 미터 단위로 입력하세요 (예: 1.75): "))
+weight = float(input("몸무게를 kg 단위로 입력하세요: "))
+
+bmi = weight / (height ** 2)
+
+print(f"당신의 BMI: {bmi:.1f}")
+if bmi < 18.5:
+    print("저체중")
+elif bmi < 25:
+    print("정상")
+elif bmi < 30:
+    print("과체중")
+else:
+    print("비만")
